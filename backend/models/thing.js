80628e436205abaf3a9947992d73ad1477892245
NicolasH67 <hecker.nicolas.pro@gmail.com>
2021-06-03 08:18:31 +0200

    modif thing.js

--- backend/models/thing.js
+++ backend/models/thing.js
@@ -1,17 +1,19 @@
 const mongoose = require('mongoose');
 
+
+//Model des sauces
 const thingSchema = mongoose.Schema({
-  name: { type: String, required: true },
-  manufacturer: { type: String, required: true },
-  description: { type: String, required: true },
-  mainPepper: { type: String, required: true },
-  imageUrl: { type: String, required: true },
-  heat: { type: Number, required: true },
-  userId: { type: String, required: true },
-  likes: { type: Number, required: true },
-  dislikes: { type: Number, required: true },
-  userLiked: { type: String, required: true },
-  userDisliked: { type: String, required: true },
+    userId: {type: String, required: true},
+    name: {type: String, required: true},
+    manufacturer: {type: String, required: true},
+    description: {type: String, required: true},
+    mainPepper: {type: String, required: true},
+    imageUrl: {type: String, required: true},
+    heat: {type: Number, required: true},
+    likes: {type: Number, required: false, default:0},
+    dislikes: {type: Number, required: false, default:0},
+    usersLiked: {type: [String], required: false},
+    usersDisliked: {type: [String], required: false},
 });
 
 module.exports = mongoose.model('Thing', thingSchema);
